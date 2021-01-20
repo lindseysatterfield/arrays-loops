4e7b31f62485a67f0cc78c66f54b6ef09136b1b4
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-19 19:43:17 -0600

    Added array exercises

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+// ***** ARRAYS *****
+const students = [
+	'Katy',
+	'Jackie',
+	'Sean',
+	'Nathan'
+];
+// console.log(students.length);
+
+// ***** ACCESSING ITEMS IN ARRAY *****
+
+// console.log(students[1]);
+// console.log(students[students.length - 1]); // Nathan // will give you the end of the index
+
+students[0] = 'Matthew';
+// console.log(students);
+
+const valuePrinter = (array, index) => {
+	return array[index];
+};
+
+// console.log(valuePrinter(students, 2));
+
+const isGregHere = (array) => {
+	return array.includes('Greg');
+};
+// console.log(isGregHere(students)); // returns false because Greg is not in the students array
