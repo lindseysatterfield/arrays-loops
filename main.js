7bde21443f122702f04ddae31a7e1bcfdb128ee4
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-01-19 20:38:19 -0600

    did some array methods

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+const array1 = [
+	'cow',
+	'dog',
+	'cat'
+];
+const string1 = array1.join();
+const string1ToArray = string1.split(',');
+// console.log(string1);
+// console.log(string1ToArray);
+
+// ***** .push() & .pop() *****
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+// ***** .unshift() & .shirt() *****
+array1.unshift('cow');
+console.log(array1);
+array1.shift('cat');
+console.log(array1);
